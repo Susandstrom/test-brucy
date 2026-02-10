23f49c7e581713f19666f052e69330d59e9966ef
Suthada <suthada@brucy.io>
2026-02-10 15:23:34 +0100

    wip11

--- app/page.tsx
+++ app/page.tsx
@@ -14,56 +14,47 @@ export default function HomePage() {
         stylePosition="40% 20%"
       />
 
-      {/* Mina recept */}
-      <section className="px-4 py-16 max-w-7xl mx-auto">
-        <h2 className="text-3xl font-bold mb-8 text-left">
-          Mina recept
-        </h2>
+      {/* Bild med text och CTA */}
+      <div className="max-w-6xl mx-auto px-4 py-12">
+        <div className="md:flex md:items-center md:gap-8">
 
-        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-          <Link
-            href="/hamburger"
-            className="group rounded-2xl bg-white p-6 shadow-md transition hover:-translate-y-1 hover:shadow-xl"
-          >
-            <img
-              src="/images/hamburger.jpg"
-              alt="Hamburger"
-              className="w-full h-48 object-cover rounded-xl mb-4"
-            />
-            <h3 className="text-xl font-semibold group-hover:text-orange-600">
-              Fredriks Burgare
-            </h3>
-          </Link>
+          {/*Vänsterbild */}
+          <img 
+          src="/images/eggs.jpg"
+          alt="Ägg i kartong"
+          className="rounded-2xl shadow-lg w-full object-cover h-70 md:h-[350px]"
+          />
 
-          <Link
-            href="/biryani"
-            className="group rounded-2xl bg-white p-6 shadow-md transition hover:-translate-y-1 hover:shadow-xl"
-          >
-            <img
-              src="/images/biryani2.jpg"
-              alt="Biryani"
-              className="w-full h-48 object-cover rounded-xl mb-4"
-            />
-            <h3 className="text-xl font-semibold group-hover:text-orange-600">
-              Elviras Biryani
-            </h3>
-          </Link>
-
-          <Link
-            href="/korvstroganof"
-            className="group rounded-2xl bg-white p-6 shadow-md transition hover:-translate-y-1 hover:shadow-xl"
-          >
-            <img
-              src="/images/stroganof.jpg"
-              alt="Korvstroganof"
-              className="w-full h-48 object-cover rounded-xl mb-4"
-            />
-            <h3 className="text-xl font-semibold group-hover:text-orange-600">
-              Klings Korvstroganof
-            </h3>
-          </Link>
+          {/*Höger: text & CTA */}
+          <div className="md:w-1/2 mt-8 md:mt-0 flex flex-col justify-center">
+          <h2 className="text-3xl font-bold mb-3">Utforska mina favoritrecept!</h2>
+          <p className="text-gray-600 mb-5">Upptäck en värld av smaker från världens olika hörn!</p>
+          <Link 
+          href="/recipe"
+          className="inline-block bg-green-600 text-white font-semibold px-6 py-3 rounded-xl shadow hover:bg-green-700 transition">Se recepten</Link>
+          </div>
         </div>
-      </section>
+      </div>
+
+      <div className="relative w-full max-w-6xl mx-auto px-4 py-12">
+        <div className="relative rounded-2xl overflow-hidden shadow-lg">
+          {/*bild med CTA i */}
+          <img
+          src="/images/pepparkorn.jpg"
+          alt="pepparkorn"
+          className="w-full h-70 md:h-[350px] object-cover"
+          />
+
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+            <h2 className="text-3xl md:text-4xl font-bold text-white mb-3 drop-shadow-lg">Kika in i vår lilla butik</h2>
+            <p className="text-white/90 mb-5 drop-shadow-md">Här hittar ni nya goda smaker eller verktyg som hjälper dig i köket</p>
+            <Link 
+            href="/shop"
+            className="bg-green-600 text-white font-semibold px-6 py-3 rounded-xl shadow hover:bg-green-700 transition">Se vår butik
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
