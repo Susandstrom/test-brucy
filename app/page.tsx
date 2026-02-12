96ea1a236420c2cce576421702de5ffa7e7017c3
Suthada <suthada@brucy.io>
2026-02-12 17:00:37 +0100

    wip13

--- app/page.tsx
+++ app/page.tsx
@@ -19,33 +19,32 @@ export default function HomePage() {
 
       {/* Bild + text + CTA */}
       <section className="max-w-6xl mx-auto px-4 pt-12 pb-6">
-        <div className="bg-white rounded-2xl shadow-md p-6 md:p-12">
-          <div className="md:flex md:items-center md:gap-12">
+          <div className="md:flex md:gap-0">
 
             {/* Bild */}
+            <div className="md:w-1/2">{/* Så bilden bara är på halva sidan */}
             <Image
               src="/images/eggs.jpg"
               alt="Ägg i kartong"
-              width={600}
-              height={600}
-              className="rounded-2xl shadow-lg w-full md:w-1/2 h-80 md:h-[400px] object-cover"
-            />
-
-            {/* Välkommen-text*/}
-            <div className="md:w-1/2 mt-8 md:mt-0">
-              <h2 className="text-3xl font-bold mb-4">
-                Upptäck nya smakrika recept och nya äventyr till köket!
-              </h2>
-              <p className="text-gray-600 mb-6">
-                Upptäck en värld av smaker från världens olika hörn med kryddor, 
-              </p>
+              width={500}
+              height={500}
+              className="w-full h-full object-cover rounded-l-2xl"
+            />{/* rounded-l-2xl betyder att det endast är rundad i hörnen på vänster sida(l=left) */}
             </div>
 
+            {/* Välkommen-text*/}
+            <div className="md:w-1/2 bg-white p-8 rounded-r-2xl flex flex-col justify-center shadow-md">
+              <h2 className="text-3xl font-bold mb-4 text-left">
+                Här hittar du smakrika recept och nya äventyr till ditt kök
+              </h2>
+              <p className="text-gray-600 mb-6 text-left">
+                Upptäck en värld av smaker från världens olika hörn med kryddor, och möjligtvis verktyg som hjälper dig i köket. Söker du efter mer så är det bara att kontakta oss!
+              </p>
           </div>
         </div>
       </section>
 
-      <section className="max-w-6xl mx-auto px-4 pt-12 pb-6">
+      <section className="max-w-6xl mx-auto px-4 pt-6 pb-6">
         <div className="flex flex-col md:flex-row md:gap-8">
 
           {/* Kort 1 */}
@@ -60,11 +59,8 @@ export default function HomePage() {
               <h3 className="text-2xl md:text-3xl font-bold text-white drop-shadow-lg mb-2">
                 Utforska mina favoritrecept
               </h3>
-              <p className="text-white/90 drop-shadow-md mb-4 max-w-xs">
-                Upptäck en värld av smaker från världens olika hörn.
-              </p>
               <a
-                href="/recipes"
+                href="/recipe"
                 className={buttonPrimary}
               >
                 Se recepten
@@ -80,13 +76,10 @@ export default function HomePage() {
               className="w-full h-80 md:h-[400px] object-cover rounded-2xl transition-transform duration-300 group-hover:scale-105"
             />
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black/25 flex flex-col justify-center items-center text-center p-4">
+            <div className="absolute inset-0 bg-black/35 flex flex-col justify-center items-center text-center p-4">
               <h3 className="text-2xl md:text-3xl font-bold text-white drop-shadow-lg mb-2">
                 Kika in i vår lilla butik
               </h3>
-              <p className="text-white/90 drop-shadow-md mb-4 max-w-xs">
-                Här hittar du nya smaker och verktyg som hjälper dig i köket.
-              </p>
               <a
                 href="/shop"
                 className={buttonSecondary}
@@ -99,7 +92,7 @@ export default function HomePage() {
       </section>
 
       {/* Bild med CTA ovanpå */}
-      <section className="max-w-6xl mx-auto px-4 pt-6 pb-12">
+      <section className="max-w-6xl mx-auto px-4 pt-6 pb-6">
         <div className="relative rounded-2xl overflow-hidden shadow-lg">
 
           <img
